70d4019cf36df2d369cff30167313367e5de2956
NexoCal <gamingnight492@gmail.com>
2024-12-16 00:54:06 +0700

    Fixed combo timer (for now atleast)

--- scripts/attackState.gd
+++ scripts/attackState.gd
@@ -10,34 +10,44 @@ var comboAttack
 func enter():
 	animplay.play("attack-1")
 	attackMove -= 1
-	$attackComboTimer.start()
-	$attackTimer.start()
+	$attackComboTimer.start(0.2)
+	$attackTimer.start(0.5)
 	resetAttack = false
-	comboAttack =true
+	
 
 func update(_delta:float):
 	if resetAttack == true:
 		stateTrans.emit(self,"idle")
 	else:
 		if Input.is_action_just_pressed("attack") and comboAttack == true:
-			if attackMove == 0:
-				stateTrans.emit(self,"idle")
 			
-			if attackMove == 3:
-				animplay.play("attack-2")
-				$attackComboTimer.start()
-				$attackTimer.start()
+			if attackMove == 4:
+				animplay.play("attack-1")
+				$attackComboTimer.start(0.2)
+				$attackTimer.start(0.5)
 				attackMove -= 1
+				comboAttack = false
+				
+			elif attackMove == 3:
+				animplay.play("attack-2")
+				$attackComboTimer.start(0.5)
+				$attackTimer.start(1.1)
+				attackMove -= 1
+				comboAttack = false
+				
 			elif attackMove == 2:
 				animplay.play("attack-3")
-				$attackComboTimer.start()
-				$attackTimer.start()
+				$attackComboTimer.start(0.2)
+				$attackTimer.start(0.7)
 				attackMove -= 1
+				comboAttack = false
+				
 			elif attackMove == 1:
 				animplay.play("attack-4")
-				$attackComboTimer.start()
-				$attackTimer.start()
-				attackMove -= 1
+				$attackComboTimer.start(0.3)
+				$attackTimer.start(0.6)
+				attackMove = 4
+				comboAttack = false
 		
 
 func exit():
@@ -53,6 +63,5 @@ func _on_attack_timer_timeout() -> void:
 
 
 func _on_attack_combo_timer_timeout() -> void:
-	comboAttack = false
-	print("ComboOut")
+	comboAttack = true
 	pass # Replace with function body.
